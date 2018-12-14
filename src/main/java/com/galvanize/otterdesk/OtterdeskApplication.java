1a6dfa84a075dc271bcb1a14422fa0ff52e3659a
vlad-vlasiuk <vlad.vlasiuk@yahoo.com>
2018-12-14 09:09:29 -0700

    create spring boot project

---
+++ src/main/java/com/galvanize/otterdesk/OtterdeskApplication.java
@@ -0,0 +1,14 @@
+package com.galvanize.otterdesk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtterdeskApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OtterdeskApplication.class, args);
+    }
+
+}
+
